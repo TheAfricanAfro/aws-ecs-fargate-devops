e3d3d092b6884418e49962bdd39e24937eea5c7a
Ahmed Abdo <mr.abdo199@gmail.com>
2026-04-11 20:37:46 -0400

    task def update

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -60,6 +60,10 @@ resource "aws_ecs_service" "project_service" {
     container_name   = "project-container"
     container_port   = 4000
   }
+  
+  lifecycle {
+    ignore_changes = [ task_definition ]
+  }
 
   depends_on = [aws_lb_listener.project_listener]
 
